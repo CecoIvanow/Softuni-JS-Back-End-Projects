50694320a1240090f9ab864a68e6a5a5dded350c
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-01-14 00:27:09 +0200

    Idented the Project

---
+++ 01. Cat Shelter/README.md
@@ -0,0 +1,10 @@
+# Cat Shelter
+
+This is a project from Softuni's Backend curriculum made for training purposes
+
+This project aims to help with understanding:
+
+- The creation of a basic Node.js server using HTTP modules without Express.js and without templating
+- The basics of communication between the client and the server
+- The Node.js core libraries such as HTTP, File System
+- Streams
---
+++ 01. Cat Shelter/src/logic/addCatLogic.js
@@ -0,0 +1,114 @@
+import { uuid } from "../../lib/uuid.js";
+import { fs, EOL, path } from "../../lib/coreLibraries.js";
+import { addCatHtml } from "../views/addCat.html.js";
+
+export function addCatLogic(req, res) {
+    const method = req.method;
+
+    switch (method) {
+        case 'POST':
+            extractInputData(req, res);
+            break;
+    }
+
+    fs.readFile('./data/breeds.json', { encoding: 'utf-8' })
+        .then(data => {
+            const breeds = JSON.parse(data);
+            res.writeHead(200, { 'content-type': 'text/html' });
+            res.write(addCatHtml(breeds));
+            res.end();
+        })
+        .catch(error => console.error(error.message));
+}
+
+function extractInputData(req) {
+    const body = [];
+
+    req.on('data', chunk => body.push(chunk));
+    req.on('end', () => {
+        const dataBuffer = Buffer.concat(body);
+        const data = dataBuffer.toString("binary");
+        const boundary = req.headers['content-type'].split('boundary=').at(1);
+        const dataParts = data.split(`--${boundary}`);
+
+        formDataRetrieval(dataParts);
+    });
+}
+
+function formDataRetrieval(dataParts) {
+    const newCatData = {};
+    const imageFile = dataParts.splice(3, 1).at(0);
+    const rawData = dataParts.splice(1, 3);
+    const catId = uuid();
+
+    newCatData[catId] = {};
+
+    const imageRetrieval = (imageFile, catData) => new Promise((resolve, reject) => {
+        const [metaData, imageData] = imageFile.split(EOL + EOL);
+        const imageName = metaData.match(/filename="(.+)"/).at(1);
+        let curDir = import.meta.dirname.split('\\');
+        let savePath = '';
+        curDir.pop();
+        curDir.pop();
+        curDir = curDir.join('\\');
+
+        fs.mkdir(`./content/images/cat_images/${catId}`)
+            .then(() => savePath = path.join(curDir, 'content', 'images', 'cat_images', catId, imageName))
+            .then(() => {
+                fs.writeFile(savePath, imageData, { encoding: 'binary' })
+                    .then(() => {
+                        catData[catId].id = catId;
+                        catData[catId].imagePath = `/content/images/cat_images/${catId}/${imageName}`;
+                        resolve();
+                    })
+                    .catch(error => {
+                        reject();
+                        throw new Error(error.message);
+                    });
+            })
+            .catch(error => console.error(error.message));
+    })
+
+    const rawDataRetrieval = (rawData, catData) => new Promise((resolve) => {
+        const [nameRawData, descriptionRawData, breedRawData] = rawData;
+
+        const name = nameRawData.split(EOL + EOL).at(1).split(EOL).at(0);
+        const description = descriptionRawData.split(EOL + EOL).at(1).split(EOL).at(0);
+        const breed = breedRawData.split(EOL + EOL).at(1).split(EOL).at(0);
+
+        catData[catId].name = name;
+        catData[catId].description = description;
+        catData[catId].breed = breed;
+        resolve();
+    })
+
+    Promise.all([
+        imageRetrieval(imageFile, newCatData),
+        rawDataRetrieval(rawData, newCatData)
+    ])
+        .then(() => {
+            initCats(newCatData)
+        })
+        .catch(error => console.error(error.message));
+}
+
+function initCats(newCat) {
+    let cats = [];
+
+    fs.readFile('./data/cats.json', { encoding: 'utf-8' })
+        .then(resp => {
+            cats = JSON.parse(resp);
+            if (cats === undefined) {
+                cats = [];
+            }
+            addNewCat(newCat, cats);
+        })
+        .catch(error => console.error(error.message));
+}
+
+function addNewCat(newCat, existingCats) {
+    existingCats.push(newCat);
+    const catsJson = JSON.stringify(existingCats, null, 2);
+    fs.writeFile('./data/cats.json', catsJson, { encoding: 'utf-8' })
+        .catch(error => console.error(error.message));
+}
