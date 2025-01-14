611db7f638292b6e022ba639f6eac7ebb845f1cb
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-01-14 19:49:04 +0200

    Added html templating using handlebars  and static file loading

--- 02. Cat Shelter w. Express and templating/src/index.js
+++ 02. Cat Shelter w. Express and templating/src/index.js
@@ -1,11 +1,32 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
 
 const app = express();
-const PORT = 5060;
+const PORT = 5000;
+
+app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
+app.set('view engine', 'hbs');
+
+app.use(express.static('content'));
 
 app.get('/', (req, res) => {
-    res.status(200);
-    res.send('It works!');
+    res.render('homePage', {layout: 'secondary'});
 })
 
-app.listen(PORT, () => console.log('Server is listening on http://localhost:5060...'))
+app.get('/cats/add-breed', (req, res) => {
+    res.render('addBreed')
+})
+
+app.get('/cats/add-cat', (req, res) => {
+    res.render('addCat')
+})
+
+app.get('/edit/:catId', (req, res) => {
+    res.render('editCat');
+})
+
+app.get('/shelter/:catId', (req, res) => {
+    res.render('catShelter');
+})
+
+app.listen(PORT, () => console.log('Server is listening on http://localhost:5000...'))
