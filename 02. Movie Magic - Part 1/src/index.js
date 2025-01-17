417069801913ebbadaf7807787cede410fc2099f
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-01-17 16:26:56 +0200

    Loaded static html home page

--- 02. Movie Magic - Part 1/src/index.js
+++ 02. Movie Magic - Part 1/src/index.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import handlebars from "express-handlebars";
 
 const app = express();
 const port = 5050;
+
+app.engine('hbs', handlebars.engine());
+
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
 
 app.listen(port, () => console.log('Server is listening on http://localhost:5050...'));
