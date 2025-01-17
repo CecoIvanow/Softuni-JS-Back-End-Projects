cc8ef2492549d6643788f46da6fd8493fe038056
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-01-18 00:03:18 +0200

    Added about page

--- 02. Movie Magic - Part 1/src/controllers/home-controller.js
+++ 02. Movie Magic - Part 1/src/controllers/home-controller.js
@@ -6,6 +6,10 @@ router.get('/', (req, res) => {
     res.render('home');
 })
 
+router.get('/about', (req, res) => {
+    res.render('about');
+})
+
 router.get('*', (req, res) => {
     res.render('404')
 })
