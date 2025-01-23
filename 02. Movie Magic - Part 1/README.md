534bf4a4d3f28bed43275fcca215b8c508c14caa
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-01-23 19:53:38 +0200

    Revert "Changed project name"

---
+++ 02. Movie Magic - Part 1/README.md
@@ -0,0 +1,7 @@
+# Cat Shelter
+
+This is a project from Softuni's Backend curriculum made for training purposes
+
+This project aims to help with understanding:
+- Express
+- Templating using express-handlebars
---
+++ 02. Movie Magic - Part 1/src/routes.js
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+import homeController from './controllers/home-controller.js'
+import movieController from './controllers/movie-controller.js'
+
+const router = Router();
+
+router.use('/movies', movieController);
+router.use(homeController);
+
+export default router;
