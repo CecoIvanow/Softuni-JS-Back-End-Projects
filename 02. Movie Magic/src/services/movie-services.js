d577b7dcec5b8d069fc231a64e1cd41a4833618c
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-01-23 20:01:37 +0200

    Changed name

---
+++ 02. Movie Magic/src/services/movie-services.js
@@ -0,0 +1,41 @@
+import movies from "../movies.js"
+import { v4 as uuid } from "uuid";
+
+function create(movieData) {
+    const id = uuid();
+    
+    movieData.id = id;
+    movieData.rating = Number(movieData.rating);
+
+    movies.push(movieData);
+}
+
+function findMovie(movieId) {
+    const movieData = movies.filter(movie => movie.id === movieId);
+
+    return movieData.at(0);
+}
+
+function getAll(filter = {}) {
+    let result = movies;
+
+    if (filter.title) {
+        result = result.filter(el => el.title.toLowerCase().includes(filter.title.toLowerCase()));    
+    }
+
+    if (filter.genre) {
+        result = result.filter(el => el.genre.toLowerCase().includes(filter.genre.toLowerCase()));
+    }
+
+    if (filter.year) {
+        result = result.filter(el => el.year === filter.year);
+    }
+
+    return result;
+}
+
+export default {
+    findMovie,
+    getAll,
+    create
+}
