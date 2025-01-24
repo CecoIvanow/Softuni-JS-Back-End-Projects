adba30ba72aae49149e5025954566213e63cd649
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-01-24 18:31:58 +0200

    Added cast controller

---
+++ 02. Movie Magic/src/controllers/cast-controller.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get('/create', (req, res) => {
+})
+
+router.post('/create', (req, res) => {
+    const castData = req.body;
+})
+
+export default router;
