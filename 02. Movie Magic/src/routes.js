c3c2edfbc957201d8f2599286a3611d704266cb4
Tsvetelin Ivanov <cvetelyn.ivanov@gmail.com>
2025-02-03 23:16:26 +0200

    Added register page

--- 02. Movie Magic/src/routes.js
+++ 02. Movie Magic/src/routes.js
@@ -2,11 +2,13 @@ import { Router } from 'express'
 import homeController from './controllers/home-controller.js'
 import movieController from './controllers/movie-controller.js'
 import castController from './controllers/cast-controller.js'
+import userController from './controllers/user-controller.js'
 
 const router = Router();
 
 router.use('/cast', castController);
 router.use('/movies', movieController);
+router.use(userController);
 router.use(homeController);
 
 export default router;
